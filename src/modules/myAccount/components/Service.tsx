0fdf55017feaf94d594c736c17a384a9f5b392b1
Alina Perevozchikova <alina@saleschamp.nl>
2023-11-09 22:28:38 +0400

    feat: optimization my account module

--- src/modules/myAccount/components/Service.tsx
+++ src/modules/myAccount/components/Service.tsx
@@ -1,5 +1,5 @@
 import { useNavigation } from '@react-navigation/native';
-import React from 'react';
+import React, { useCallback } from 'react';
 import {
   Dimensions,
   Image,
@@ -12,21 +12,31 @@ import { WebCardWrapper } from '../../../ui/WebCardWrapper';
 
 const { width } = Dimensions.get('window');
 
-export const Service: React.FC<any> = ({ service }) => {
-  const navigation: any = useNavigation();
+export interface IService {
+  id: string;
+  title: string;
+  image: number;
+  redirectPage: string;
+}
 
-  return (
-    <WebCardWrapper cardWidth={width}>
-      <TouchableOpacity
-        style={styles.container}
-        onPress={() => navigation.navigate(service.redirectPage)}
-      >
-        <Image source={service.image} style={styles.image} />
-        <Text style={styles.text}>{service.title}</Text>
-      </TouchableOpacity>
-    </WebCardWrapper>
-  );
-};
+export const Service: React.FC<{ service: IService }> = React.memo(
+  ({ service }) => {
+    const navigation: any = useNavigation();
+
+    const onPress = useCallback(() => {
+      navigation.navigate(service.redirectPage);
+    }, [navigation, service.redirectPage]);
+
+    return (
+      <WebCardWrapper cardWidth={width}>
+        <TouchableOpacity style={styles.container} onPress={onPress}>
+          <Image source={service.image} style={styles.image} />
+          <Text style={styles.text}>{service.title}</Text>
+        </TouchableOpacity>
+      </WebCardWrapper>
+    );
+  }
+);
 
 const styles = StyleSheet.create({
   container: {
